7a0b249f6f3fb9a43586e7cfc5703db04d49ebef
kalyan524 <kalyansaiteja99@gmail.com>
2026-07-16 12:59:38 +0530

    aks size change

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name           = "default"
     node_count     = 1
-    vm_size        = "Standard_B2s"
+    vm_size        = "Standard_D2as_v5"
     vnet_subnet_id = var.aks_subnet_id
   }
 
