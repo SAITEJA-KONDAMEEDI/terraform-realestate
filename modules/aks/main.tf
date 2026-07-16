b7a86086d2c859123abebccd3b0d0b2f923c7ab6
kalyan524 <kalyansaiteja99@gmail.com>
2026-07-16 11:43:37 +0530

    fix aks changes

---
+++ modules/aks/main.tf
@@ -0,0 +1,24 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "sai-realestate-aks"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = "saireal"
+
+  default_node_pool {
+    name           = "default"
+    node_count     = 1
+    vm_size        = "Standard_B2s"
+    vnet_subnet_id = var.aks_subnet_id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = var.acr_id
+  skip_service_principal_aad_check = true
+}
