a4b0a11b98a794712bb363025c4dc31674c7a6f8
kalyan524 <kalyansaiteja99@gmail.com>
2026-07-17 18:22:11 +0530

    realestate pipeline

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "RealEstate-rg"
-    storage_account_name = "realestatetfstate123"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
